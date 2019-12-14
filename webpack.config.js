895683a47608f979c390b70c821f19ff55f9a43b
MaksimKazak <kazakmaksims@gmail.com>
2019-12-15 00:41:15 +0300

    Add babel typescript and configure them

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,28 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/index.ts',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
   },
-  mode: 'development'
+  mode: 'development',
+  watch: true,
+  devtool: 'source-map',
+
+  module: {
+    rules: [
+      {
+        test: [/\.js$/, /\.ts$/],
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  }
 };
