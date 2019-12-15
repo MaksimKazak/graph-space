29033f59447acfe16611a5f7b970d17aa27cd5a1
MaksimKazak <kazakmaksims@gmail.com>
2019-12-15 15:40:08 +0300

    Add vue and configure it eith ts

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+    <div id='mynetwork'></div>
+  </div>
+</template>
+
+<script lang="ts">
+import vis from 'vis-network';
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  mounted () {
+      console.log('mounted');
+    let nodes = new vis.DataSet([
+        {id: 1, label: 'Node 1', color: '#756338'},
+        {id: 2, label: 'Node 2'},
+        {id: 3, label: 'Node 3'},
+        {id: 4, label: 'Node 4'},
+        {id: 5, label: 'Node 5'}
+    ]);
+
+    let edges = new vis.DataSet([
+        {from: 1, to: 3, color: '#277dea'},
+        {from: 1, to: 2, color: '#277dea'},
+        {from: 2, to: 4, color: '#277dea'},
+        {from: 2, to: 5, color: '#277dea'},
+    ]);
+
+    let container = document.getElementById('mynetwork');
+    let data = {
+        nodes: nodes,
+        edges: edges
+    };
+    let options = {};
+    let network = new vis.Network(container, data, options);
+
+    network.on('selectNode', event => {
+        console.log('select', event);
+    });
+    network.on('deselectNode', event => {
+        console.log('deselect ', event);
+    });
+  }
+}
+</script>
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
 
 module.exports = {
   entry: './src/index.ts',
@@ -13,16 +14,45 @@ module.exports = {
   module: {
     rules: [
       {
-        test: [/\.js$/, /\.ts$/],
+        test: /\.js$/,
         exclude: /node_modules/,
         use: {
           loader: "babel-loader"
         },
       },
       {
+        test: /\.ts$/,
+        exclude: /node_modules|vue\/src/,
+        loader: 'ts-loader',
+        options: {
+          appendTsSuffixTo: [/\.vue$/]
+         }
+      },
+      {
         test: /\.css$/,
         use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: {
+          loaders: {
+            'scss': [
+              'vue-style-loader',
+              'css-loader',
+              'sass-loader'
+            ],
+            'sass': [
+              'vue-style-loader',
+              'css-loader',
+              'sass-loader?indentedSyntax'
+            ]
+          }
+        }
       }
     ]
-  }
+  },
+  plugins: [
+    new VueLoaderPlugin()
+  ]
 };
