6d8ed00fd50b7605d9ea7273dd2120239513c2f4
Maksim Kazak <m.kazak@192.168.1.37>
2020-03-19 20:25:36 +0300

    Add config for base graphs

---
+++ src/config/radicals.js
@@ -0,0 +1,179 @@
+const radicals = [
+  {
+    id: 1,
+    nodes: [
+      {
+        id: 1,
+        label: 'C',
+      },
+      {
+        id: 2,
+        label: 'H',
+      },
+      {
+        id: 3,
+        label: 'H',
+      },
+      {
+        id: 4,
+        label: 'H',
+      },
+    ],
+    edges: [
+      {
+        from: 2,
+        to: 1,
+      },
+      {
+        from: 3,
+        to: 1,
+      },
+      {
+        from: 4,
+        to: 1,
+      },
+    ],
+  },
+  {
+    id: 2,
+    nodes: [
+      {
+        id: 5,
+        label: 'C',
+      },
+      {
+        id: 6,
+        label: 'C',
+      },
+      {
+        id: 7,
+        label: 'H',
+      },
+      {
+        id: 8,
+        label: 'H',
+      },
+      {
+        id: 9,
+        label: 'H',
+      },
+      {
+        id: 10,
+        label: 'H',
+      },
+      {
+        id: 11,
+        label: 'H',
+      },
+    ],
+    edges: [
+      {
+        from: 6,
+        to: 5,
+      },
+      {
+        from: 7,
+        to: 5,
+      },
+      {
+        from: 8,
+        to: 5,
+      },
+      {
+        from: 9,
+        to: 6,
+      },
+      {
+        from: 10,
+        to: 6,
+      },
+      {
+        from: 11,
+        to: 6,
+      },
+    ],
+  },
+  {
+    id: 3,
+    nodes: [
+      {
+        id: 12,
+        label: 'C',
+      },
+      {
+        id: 13,
+        label: 'C',
+      },
+      {
+        id: 14,
+        label: 'C',
+      },
+      {
+        id: 15,
+        label: 'H',
+      },
+      {
+        id: 16,
+        label: 'H',
+      },
+      {
+        id: 17,
+        label: 'H',
+      },
+      {
+        id: 18,
+        label: 'H',
+      },
+      {
+        id: 19,
+        label: 'H',
+      },
+      {
+        id: 20,
+        label: 'H',
+      },
+      {
+        id: 21,
+        label: 'H',
+      },
+    ],
+    edges: [
+      {
+        from: 13,
+        to: 12,
+      },
+      {
+        from: 14,
+        to: 12,
+      },
+      {
+        from: 15,
+        to: 12,
+      },
+      {
+        from: 16,
+        to: 13,
+      },
+      {
+        from: 17,
+        to: 13,
+      },
+      {
+        from: 18,
+        to: 13,
+      },
+      {
+        from: 19,
+        to: 14,
+      },
+      {
+        from: 20,
+        to: 14,
+      },
+      {
+        from: 21,
+        to: 14,
+      },
+    ],
+  },
+];
